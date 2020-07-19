63580dc7af017cc9fe18784414aabdd2e22c6ed1
Yugesh <yugeshkeluskar.yk@gmail.com>
2020-07-20 01:28:58 +0530

    Intro scene

--- projects/convolution/convolution.py
+++ projects/convolution/convolution.py
@@ -2,7 +2,7 @@
 
 from big_ol_pile_of_manim_imports import *
 
-class title(Scene):
+class Intro(Scene):
     def construct(self):
         NameSlide = TextMobject("Intuition around Convolution",tex_to_color_map={"Convolution": YELLOW})
         NameSlide.scale(2)
