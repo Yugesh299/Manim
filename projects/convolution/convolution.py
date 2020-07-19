6e28f4e42cf80731ab85ac81631b7d83e5f18b80
Yugesh <yugeshkeluskar.yk@gmail.com>
2020-07-20 01:17:36 +0530

    Test

---
+++ projects/convolution/convolution.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from big_ol_pile_of_manim_imports import *
+
+class title(Scene):
+    def construct(self):
+        NameSlide = TextMobject("Intuition around Convolution",tex_to_color_map={"Convolution": YELLOW})
+        NameSlide.scale(2)
+
+        self.play(Write(NameSlide))
+        self.wait(2)
---
+++ tutorials/all_classes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from big_ol_pile_of_manim_imports import *
+
+
+class logo(Scene):
+    def construct(self):
+        logo  = TextMobject("Intuitio")
+        logo.scale(2)
+
+        self.play(Write(logo))
+        self.wait(1)
